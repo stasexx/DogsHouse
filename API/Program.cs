e1396da19d10d96b729cf6233e335a5098cf0ccc
stasex <stasek.molchan@gmail.com>
2023-10-11 00:11:55 +0300

    some fix for database and creat first migration

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
