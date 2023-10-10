95fd582e9b226ee8d3861dae2ef378d56f6b5be1
stasex <stasek.molchan@gmail.com>
2023-10-10 23:18:31 +0300

    added dog entity

---
+++ Domain/Entities/Dog.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities;
+
+public class Dog
+{
+    public string Name { get; set; }
+    
+    public string Color { get; set; }
+    
+    public double TailLength { get; set; }
+    
+    public double Weight { get; set; }
+}
